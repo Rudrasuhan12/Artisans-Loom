92c094dc8993be407d7925fffc12254d15bb3002
Rudrasuhan12 <rudranarayanray1126@gmail.com>
2026-03-03 16:45:19 +0530

    fixed the proxy

--- client/src/proxy.ts
+++ client/src/proxy.ts
@@ -1,7 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/artisan(.*)',
+  '/artisan',
+  '/artisan/(.*)',
   '/customer(.*)',
   '/admin(.*)',
   '/dashboard(.*)',
